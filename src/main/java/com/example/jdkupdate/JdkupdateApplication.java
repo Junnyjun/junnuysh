33f05f58d22148c6582443f7d69d389326f1f59c
JunnyJun <chbe5082@naver.com>
2023-06-08 00:11:56 +0900

    first commit

---
+++ src/main/java/com/example/jdkupdate/JdkupdateApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdkupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdkupdateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdkupdateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jdkupdate/JdkupdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdkupdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdkupdateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
