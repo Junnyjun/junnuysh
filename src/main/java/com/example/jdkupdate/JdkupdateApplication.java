d90eeb44ccf468644046e177bfd5c0414c1f4414
JunnyJun <chbe5082@naver.com>
2023-06-13 23:19:21 +0900

    banner

--- src/main/java/com/example/jdkupdate/JdkupdateApplication.java
+++ src/main/java/com/example/jdkupdate/JdkupdateApplication.java
@@ -1,13 +1,18 @@
 package com.example.jdkupdate;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JdkupdateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JdkupdateApplication.class, args);
+		SpringApplication app = new SpringApplication(JdkupdateApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);   //요 부분
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
 	}
 
 }
--- src/main/java/com/example/jdkupdate/global/LogConfig.java
+++ src/main/java/com/example/jdkupdate/global/LogConfig.java
@@ -1,23 +1,20 @@
 package com.example.jdkupdate.global;
 
-import org.springframework.boot.Banner;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.core.env.Environment;
-import org.springframework.shell.boot.SpringShellProperties;
-import org.springframework.shell.standard.commands.History;
 import org.springframework.stereotype.Component;
-
-import java.io.PrintStream;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class LogConfig implements Banner {
+public class LogConfig implements ApplicationListener<ApplicationStartedEvent> {
+
     @Override
-    public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
-        out.println("======================");
-        out.println("=WELTOME TO JUNNYLAND=");
-        out.println("=BASH SHELL          =");
-        out.println("======================");
+    public void onApplicationEvent(ApplicationStartedEvent event) {
+        System.out.println("======================");
+        System.out.println("=WELTOME TO JUNNYLAND=");
+        System.out.println("=BASH SHELL          =");
+        System.out.println("======================");
     }
 }
